95a13204db3f8b281a4a1126e593be1e42486cbe
samyuktha1232 <s.a.sannapureddy@accenture.com>
2025-08-07 09:20:04 +0530

    Create simple_hello_world_dag.py

---
+++ simple_hello_world_dag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+with DAG(
+    dag_id='simple_hello_world_dag_samyuktha', # A new, unique DAG ID
+    start_date=datetime(2023, 1, 1), # A static start date in the past
+    schedule_interval=None, # Set to None for manual trigger
+    catchup=False,
+    tags=['test', 'hello_world'],
+) as dag:
+    # Define a simple task that just prints a message
+    hello_task = BashOperator(
+        task_id='say_hello',
+        bash_command='echo "Hello from Samyuktha\'s simple DAG!"',
+    )
