65f61f6dc4dcb0aea91365148d9e0edf5ad680c6
samyuktha1232 <s.a.sannapureddy@accenture.com>
2025-08-07 09:04:31 +0530

    Update simple_hello_world_dag.py

--- dags/simple_hello_world_dag.py
+++ dags/simple_hello_world_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 with DAG(
-    dag_id='simple_hello_world_dag_samyuktha', # A new, unique DAG ID
+    dag_id='simple_hello_world_dag', # A new, unique DAG ID
     start_date=datetime(2023, 1, 1), # A static start date in the past
     schedule_interval=None, # Set to None for manual trigger
     catchup=False,
